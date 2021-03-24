7b796bca539a76d84c2eea29fe66cd53abc54d4a
RossSihovsk <rsihovskiy@gmail.com>
2021-03-24 17:48:17 +0200

    Final update(i think)

--- Application.js
+++ Application.js
@@ -1,44 +1,67 @@
-getAge.onblur = function() {
-    if ((this.value < 0) || (this.value > 100) || (isNaN(this.value))) {
-        document.getElementById("Message").style = "visible";
-        document.getElementById("AjaxGET").disabled = true;
-        document.getElementById("AjaxPOST").disabled = true;
-        getAge.classList.add('error');
-    } else {
+function check() {
+    let surname = document.getElementById("Surname").value;
+    let name = document.getElementById("Name").value;
+    let age = document.getElementById("Age").value;
+    let address = document.getElementById("Address").value;
+
+    if (surname && name && (age > 0 && age <= 100) && address) {
         document.getElementById("Message").style.visibility = "hidden";
         document.getElementById("AjaxGET").disabled = false;
         document.getElementById("AjaxPOST").disabled = false;
-        getAge.classList.remove('error');
+    } else {
+        document.getElementById("Message").style.visibility = "visible";
+        document.getElementById("AjaxGET").disabled = true;
+        document.getElementById("AjaxPOST").disabled = true;
     }
 }
 
+function validateAge() {
+    let age = document.getElementById("Age");
+
+    if (age.value > 0 && age.value <= 100) {
+        age.classList.remove("error");
+    } else {
+        age.classList.add("error");
+        age.value = "";
+        age.focus();
+        console.log("Возраст должен быть числом от 1 до 100 лет!");
+    }
+}
 
 function sendDataByGetMethod() {
     let userData = {
-        lName : document.getElementById('getLastName').value,
-        fName : document.getElementById('getFirstName').value,
-        age : document.getElementById('getAge').value,
-        address : document.getElementById('getAddress').value
+        userSurname : document.getElementById("Surname").value,
+        userName : document.getElementById("Name").value,
+        userAge : document.getElementById("Age").value,
+        userAddress : document.getElementById("Address").value
     };
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/userGet?lName=" + userData.lName + "&fName="
-        + userData.fName + "&age=" + userData.age + "&address="
-        + userData.address);
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/userGet?Surname=" + userData.userSurname + "&Name="
+        + userData.userName + "&Age=" + userData.userAge + "&Address="
+        + userData.userAddress);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send();
 }
 
 function sendDataByPostMethod() {
     let userData = {
-        lName : document.getElementById('getLastName').value,
-        fName : document.getElementById('getFirstName').value,
-        age : document.getElementById('getAge').value,
-        address : document.getElementById('getAddress').value
+        userSurname : document.getElementById("Surname").value,
+        userName : document.getElementById("Name").value,
+        userAge : document.getElementById("Age").value,
+        userAddress : document.getElementById("Address").value
     };
 
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", "/userPost");
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(userData));
+}
+
+function clearFields() {
+    document.getElementById("Surname").value = "";
+    document.getElementById("Name").value = "";
+    document.getElementById("Age").value = "";
+    document.getElementById("Address").value = "";
+    check();
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var bodyParser = require("body-parser");
+let express = require("express");
+let bodyParser = require("body-parser");
 
-var server = express();
-var jsonParser = bodyParser.json();
+let server = express();
+let jsonParser = bodyParser.json();
 server.use(jsonParser);
 
 server.use(express.static(__dirname));
@@ -11,20 +11,22 @@ server.get("/", function (req, resp) {
     console.log("Start page requested.");
 });
 
-server.get("/formGet", function (req, resp) {
+server.get("/userGet", function (req, resp) {
+
     console.log("Form receipted by GET.");
-    var obj = req.query;
+    let obj = req.query;
     console.log(obj.lName += ".ValidatedByGET");
     console.log(obj.fName += ".ValidatedByGET");
     console.log(obj.age += ".ValidatedByGET");
     console.log(obj.address += ".ValidatedByGET");
-	resp.send(obj);
+    resp.send(obj);
 });
 
-server.post("/formPost", function (req, resp) {
+server.post("/userPost", function (req, resp) {
     console.log("Form receipted by POST.");
+    crossOrigin: null
     // console.log(JSON.parse(req.query));
-    var obj = req.body;
+    let obj = req.body;
     console.log(obj.lName += ".ValidatedByPOST");
     console.log(obj.fName += ".ValidatedByPOST");
     console.log(obj.age += ".ValidatedByPOST");
